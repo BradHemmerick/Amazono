ba121cf9a3edcab4fa235c65b0812657d0e086fd
BradHemmerick <Brad.hemmerick13@gmail.com>
2018-04-09 18:51:48 -0500

    bug fix

--- client/src/app/auth-guard.service.ts
+++ client/src/app/auth-guard.service.ts
@@ -1,6 +1,5 @@
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { stat } from 'fs';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
